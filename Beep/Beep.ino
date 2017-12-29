b78226ede7c2558cd0c9e8c64b5c2a6e4d65eb3c
Claus Kühnel <info@ckuehnel.ch>
2017-12-29 12:13:23 +0100

    Add files via upload

--- Beep/Beep.ino
+++ Beep/Beep.ino
@@ -14,15 +14,15 @@
 const int Piezo = D0;
 const unsigned int LoMax = 3000;
 const unsigned int LocMin = 3500;
-const unsigned int HiMax = 4200;
+const unsigned int HiMax = 4300;
 
 // Generation of beep tone
 void beep(unsigned int pitch, unsigned long duration)
 {
-    digitalWrite(LED_BUILTIN, LOW);
-    tone(Piezo, pitch, duration);
-    delay(1.3 * duration);
-    digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
+  tone(Piezo, pitch, duration);
+  delay(1.3 * duration);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void setup() 
@@ -31,29 +31,31 @@ void setup()
   digitalWrite(LED_BUILTIN, HIGH);
   
   Serial.begin(115200);
-  delay(2000);
-  Serial.println("Play different beep tones...");
+  delay(4000);
 }
 
 void loop() 
 {
+  Serial.println("Play different beep tones...");
   Serial.println("Pitch a'");
   beep(NOTE_A4, 1000);
   Serial.println("Short Melody");
-  beep(NOTE_C5, 1000);
-  beep(NOTE_E5, 1000);
-  beep(NOTE_G5, 1000);
+  beep(NOTE_C5, 500);
+  beep(NOTE_E5, 500);
+  beep(NOTE_G5, 500);
   beep(NOTE_C6, 1000);
   Serial.println("Piezo frequency response");
-  Serial.println("       /\ ");
-  Serial.println("  /\  /  \ ");
-  Serial.println(" /  --    \ ");
-  Serial.println("/          \ ");
-  Serial.println("Low Max = ");
+  Serial.println("        /\\ ");
+  Serial.println("  /\\   /  \\ ");
+  Serial.println(" /  --     \\ ");
+  Serial.println("/           \\ ");
+  Serial.print("Low Max = "); Serial.println(LoMax);
   beep(LoMax, 1000);
-  Serial.println("Local Min = ");
+  Serial.print("Local Min = "); Serial.println(LocMin);
   beep(LocMin, 1000);
-  Serial.println("High Max = ");
-  beep(HiMax, 1000);  
+  Serial.print("High Max = "); Serial.println(HiMax);
+  beep(HiMax, 1000); 
+  noTone(Piezo);
+  Serial.println(); 
   delay(2000);
 }
